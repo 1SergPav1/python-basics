467abfefe8e523f593f5f08b30e70367b9e94bb4
Sergey Pav <98906764+1SergPav1@users.noreply.github.com>
2022-02-14 01:22:25 +0300

    Delete Pavlenko_Sergey_dz_1 directory

--- Pavlenko_Sergey_dz_1/task_1_3.py
+++
@@ -1,9 +0,0 @@
-Задание 3
-
-for num in range(1, 101):
-    if num % 10 == 1 and num not in range(11, 21):
-        print(num, 'процент')
-    elif num % 10 in range(2, 5) and num not in range(11, 21):
-        print(num, 'процента')
-    else:
-        print(num, 'процентов')
